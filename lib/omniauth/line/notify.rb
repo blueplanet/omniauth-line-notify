adb28a63e10cdbce110e5d914080ad71f6ef4d0c
blueplanet <erguolinge@gmail.com>
2016-10-05 10:15:42 +0900

    init

---
+++ lib/omniauth/line/notify.rb
@@ -0,0 +1,9 @@
+require "omniauth/line/notify/version"
+
+module Omniauth
+  module Line
+    module Notify
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/omniauth/line/notify/version.rb
@@ -0,0 +1,7 @@
+module Omniauth
+  module Line
+    module Notify
+      VERSION = "0.1.0"
+    end
+  end
+end
