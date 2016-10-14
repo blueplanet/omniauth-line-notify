b472a03391ef8bcc56f6089811a50dbf0ebe9504
blueplanet <erguolinge@gmail.com>
2016-10-14 20:57:46 +0900

    fix url, remove raw_info

--- lib/omniauth/strategies/line-notify.rb
+++ lib/omniauth/strategies/line-notify.rb
@@ -6,27 +6,10 @@ module OmniAuth
     class LineNotify < OmniAuth::Strategies::OAuth2
       option :name, 'line_notify'
 
-      option :client_options, {:site  => 'https://notify­bot.line.me',
+      option :client_options, {:site  => 'https://notify-bot.line.me/',
                                :authorize_url => '/oauth/authorize',
                                :token_url     => '/oauth/token',
                                :proxy => ENV['http_proxy'] ? URI(ENV['http_proxy']) : nil}
-
-      uid { raw_info['mid'] }
-
-      info do
-        {
-          name:        raw_info['displayName'],
-          image:       raw_info['pictureUrl'],
-          description: raw_info['statusMessage']
-        }
-      end
-
-      # Require: Access token with PROFILE permission issued.
-      def raw_info
-        @raw_info ||= JSON.load(access_token.get('v1/profile').body)
-      rescue ::Errno::ETIMEDOUT
-        raise ::Timeout::Error
-      end
 
       def authorize_params
         super.tap do |params|
