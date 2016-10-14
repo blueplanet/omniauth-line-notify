0acd0fdceb79b982fda5f127f38f9dd64f95901c
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2016-10-14 12:05:06 +0700

    update host name for line-notify server

--- lib/omniauth/strategies/line-notify.rb
+++ lib/omniauth/strategies/line-notify.rb
@@ -6,7 +6,7 @@ module OmniAuth
     class LineNotify < OmniAuth::Strategies::OAuth2
       option :name, 'line_notify'
 
-      option :client_options, {:site  => 'https://notify­bot.line.me',
+      option :client_options, {:site  => 'https://notify-bot.line.me,
                                :authorize_url => '/oauth/authorize',
                                :token_url     => '/oauth/token',
                                :proxy => ENV['http_proxy'] ? URI(ENV['http_proxy']) : nil}
